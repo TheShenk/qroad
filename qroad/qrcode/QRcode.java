50156ce5cbd73e73f962cfab936eabf33cd7f136
TheShenk <jrsutton085@gmail.com>
2018-10-22 20:31:59 +0300

    Add basic of QRcode class

--- qroad/qrcode/QRcode.java
+++ qroad/qrcode/QRcode.java
@@ -1,12 +1,15 @@
-package com.qwertu.qroad.QRcode;
+package com.qwerty.qroad.QRcode;
 
 class QRcode{
 
+    //В QR коде храним уникальный id здания, номер этажа, на котором находится QR, и номер капчи. Позже по этим
+    // данным ищем карту и информацию для отображения местоположения
+
     private int house_id;
     private int floor;
-    private int qrcode_id;
+    private int captcha_id;
 
-    QRcode() {
+    QRcode(Image image) {
 
     }
 
