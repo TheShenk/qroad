9c621564ffb78ec57913a497b3d5699db466036f
syncended <syncended@gmail.com>
2018-10-25 21:59:42 +0300

    Last version

---
+++ qroad/android_project/src/com/qwerty/qroad/CameraActivity.java
@@ -0,0 +1,78 @@
+package com.qwerty.qroad;
+
+import android.annotation.SuppressLint;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder holder;
+    private BarcodeDetector detector;
+    private CameraSource source;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        detector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        source = new CameraSource
+                .Builder(this, detector)
+                .build();
+        holder = surfaceView.getHolder();
+        holder.addCallback(new CameraView());
+
+        
+        //Еще работаю над этой фигней с qr кодами (Библиотека в gradle)
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+
+            }
+        });
+    }
+
+    private class CameraView implements SurfaceHolder.Callback {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                source.start(holder);
+            } catch (IOException e) {
+                Toast.makeText(CameraActivity.this, "Camera error", Toast.LENGTH_LONG).show();
+                System.exit(0);
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            source.stop();
+        }
+    }
+
+}
--- qroad/android_project/src/com/qwerty/qroad/MainActivity.java
+++ qroad/android_project/src/com/qwerty/qroad/MainActivity.java
@@ -1,19 +1,37 @@
 package com.qwerty.qroad;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPremission();
+    }
 
+    @SuppressLint("NewApi")
+    private void checkPremission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CODE);
     }
 
     public void startButton(View view) {
-        finish();
+        Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+        startActivity(intent);
     }
+
 }
