f2c2374a1629a1972b41183c119630fbab510c59
TheShenk <jrsutton085@gmail.com>
2018-10-26 10:11:13 +0300

    Add reading of svg and json

--- qroad/android_project/src/com/qwerty/qroad/genImage/MapImage.java
+++ qroad/android_project/src/com/qwerty/qroad/genImage/MapImage.java
@@ -1,16 +1,53 @@
 package com.qwerty.qroad.genImage;
 
 import com.qwerty.qroad.QRcode.*;
-import com.qwerty.qroad.Map.*;
 
-class MapImage{
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
-    MapImage(QRcode qRcode) {
+import com.larvalabs.svgandroid.SVG;
 
-        String wayToFile = qRcode.toFilePath();
+public static class MapImage{
+
+    MapImage(String wayToFile) {
+
+        String wayToFile = "/home/shenk/qroad/qroad/mapData/0/0/map_data";
+
+        MapData mapData = new MapData(wayToFile);
+
+    }
+
+}
+
+class MapData{
+
+    private SVG svgMap;
+
+    MapData(String wayToFile){
+
+        String jsonStringMap = readFile(wayToFile);
+
+        JSONObject jsonMap = (JSONObject)JSONValue.parse(jsonStringMap);
+
+        svgMap = SVG()
+
+
+    }
+
+    private static String readFile(String wayToFile) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(wayToFile)));
+    }
+
+}
+
+class VectorImage{
+
+    VectorImage(String wayToFile){
 
 
 
     }
 
 }
+
+
